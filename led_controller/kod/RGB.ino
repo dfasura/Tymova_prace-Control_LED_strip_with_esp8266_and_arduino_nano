c60cf5f66f49d6f720030675b5335c0df452a578
Dominik Fasura <fasura.dominik@sspbrno.cz>
2019-05-20 18:44:04 +0200

    Oprava kod bez delay

--- led_controller/kod/RGB.ino
+++ led_controller/kod/RGB.ino
@@ -1,149 +1,171 @@
 #include <Adafruit_NeoPixel.h>
 
-#define PIN 6
-#define BRIGHTNESS 20
+#define PIXELSPIN   2
+#define NUMPIXELS   8
+#define CALIBRATIONTIME 20000
 
-Adafruit_NeoPixel MagicLED = Adafruit_NeoPixel(24, PIN, NEO_GRB + NEO_KHZ800);
-const int ledPin =  6 ;
-int ledState = LOW ;
-unsigned long previousMillis = 0 ;
-const long interval = 100;
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIXELSPIN, NEO_GRB + NEO_KHZ800);
+
+unsigned long pixelsInterval=50;
+unsigned long colorWipePreviousMillis=0;
+unsigned long theaterChasePreviousMillis=0;
+unsigned long theaterChaseRainbowPreviousMillis=0;
+unsigned long rainbowPreviousMillis=0;
+unsigned long rainbowCyclesPreviousMillis=0;
+
+int theaterChaseQ = 0;
+int theaterChaseRainbowQ = 0;
+int theaterChaseRainbowCycles = 0;
+int rainbowCycles = 0;
+int rainbowCycleCycles = 0;
+
+uint16_t currentPixel = 0;
 
 void setup() {
-  pinMode ( 6 , OUTPUT ) ; 
-  MagicLED.begin();
-  MagicLED.show();
-  MagicLED.setBrightness(BRIGHTNESS);
+  currentPixel = 0;
+  
+  pixels.begin();
+  pixels.show();
+      
 }
 
-void loop() {
-  //colorWipe(MagicLED.Color(255, 5, 180), interval); //Pink
-  //colorWipe(MagicLED.Color(255, 0, 0), interval); // Red
-  //colorWipe(MagicLED.Color(255, 150, 0), interval); // Orange
-  //colorWipe(MagicLED.Color(255, 255, 5), interval); // Yellow
-  //colorWipe(MagicLED.Color(0, 255, 0), interval); // Green
-  //colorWipe(MagicLED.Color(0, 0, 255), interval); // Blue
-  //colorWipe(MagicLED.Color(135, 10, 215), interval); // Purple
-  //theaterChase(MagicLED.Color(127, 127, 127), interval); // White
-  //theaterChase(MagicLED.Color(127, 0, 0), interval); // Red
-  //theaterChase(MagicLED.Color(0, 0, 127), interval); // Blue
-  //duha(interval);
-  //strips();
-  //y();
-  //rainbowCycle(interval);
-  unsigned long currentMillis = millis();
+void loop () {
+  
+        if ((unsigned long)(millis() - colorWipePreviousMillis) >= pixelsInterval) {
+          colorWipePreviousMillis = millis();
+          colorWipe(pixels.Color(255,0,0)); //red
+        }
+  
+      if ((unsigned long)(millis() - colorWipePreviousMillis) >= pixelsInterval) {
+          colorWipePreviousMillis = millis();
+          colorWipe(pixels.Color(0,255,0)); //green
+        }
+        
+      if ((unsigned long)(millis() - colorWipePreviousMillis) >= pixelsInterval) {
+          colorWipePreviousMillis = millis();
+          colorWipe(pixels.Color(0,0,255)); //blue
+        }
+  
+      if ((unsigned long)(millis() - colorWipePreviousMillis) >= pixelsInterval) {
+          colorWipePreviousMillis = millis();
+          colorWipe(pixels.Color(255, 5, 180)); //pink
+        }
+  
+      if ((unsigned long)(millis() - colorWipePreviousMillis) >= pixelsInterval) {
+          colorWipePreviousMillis = millis();
+          colorWipe(pixels.Color(255, 150, 0)); //orange
+        }
+  
+        if ((unsigned long)(millis() - theaterChasePreviousMillis) >= pixelsInterval) {
+          theaterChasePreviousMillis = millis();
+          theaterChase(pixels.Color(127, 127, 127)); //White
+        }
+  
+      if ((unsigned long)(millis() - theaterChasePreviousMillis) >= pixelsInterval) {
+          theaterChasePreviousMillis = millis();
+          theaterChase(pixels.Color(255, 0, 0)); //red
+        }
+  
+      if ((unsigned long)(millis() - theaterChasePreviousMillis) >= pixelsInterval) {
+          theaterChasePreviousMillis = millis();
+          theaterChase(pixels.Color(0, 255, 0)); //green
+        }
+  
+      if ((unsigned long)(millis() - theaterChasePreviousMillis) >= pixelsInterval) {
+          theaterChasePreviousMillis = millis();
+          theaterChase(pixels.Color(0, 0, 255)); //blue
+        }
+      
+        if ((unsigned long)(millis() - theaterChaseRainbowPreviousMillis) >= pixelsInterval) {
+          theaterChaseRainbowPreviousMillis = millis();
+          theaterChaseRainbow();
+        }
+      
+        if ((unsigned long)(millis() - rainbowPreviousMillis) >= pixelsInterval) {
+          rainbowPreviousMillis = millis();
+          rainbow();
+        }
+      
+        if ((unsigned long)(millis() - rainbowCyclesPreviousMillis) >= pixelsInterval) {
+            rainbowCyclesPreviousMillis = millis();
+            rainbowCycle();
+        }  
 
-  if (currentMillis - previousMillis >= interval) {
-    // save the last time you blinked the LED
-    previousMillis = currentMillis;
+}
 
-    // if the LED is off turn it on and vice-versa:
-    if (ledState == LOW) {
-      ledState = HIGH;
-    } else {
-      ledState = LOW;
-    }
 
-    // set the LED with the ledState of the variable:
-    digitalWrite(ledPin, ledState);
+void colorWipe(uint32_t c){
+  pixels.setPixelColor(currentPixel,c);
+  pixels.show();
+  currentPixel++;
+  if(currentPixel == NUMPIXELS){
+    currentPixel = 0;
   }
 }
 
-void y () {
-  vymaluj(MagicLED.Color(255, 0, 0), interval); //červená
-  vymaluj(MagicLED.Color(0, 255, 0), interval); //zelená
-  vymaluj(MagicLED.Color(0, 0, 255), interval); //modrá
-}
-
-void strips() {
-  utikej(MagicLED.Color(127, 127, 127), interval); //bíla
-  utikej(MagicLED.Color(127,   0,   0), interval); //červená
-  utikej(MagicLED.Color(  0,   0, 127), interval); //modrá
-}
-
-void duha(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256; j++) {
-    for(i=0; i<MagicLED.numPixels(); i++) {
-      MagicLED.setPixelColor(i, Wheel((i+j) & 255));
-    }
-    MagicLED.show();
-    delay(wait);             
+void rainbow() {
+  for(uint16_t i=0; i<pixels.numPixels(); i++) {
+    pixels.setPixelColor(i, Wheel((i+rainbowCycles) & 255));
   }
+  pixels.show();
+  rainbowCycles++;
+  if(rainbowCycles >= 256) rainbowCycles = 0;
 }
+
+
+void rainbowCycle() {
+  uint16_t i;
+
+
+    for(i=0; i< pixels.numPixels(); i++) {
+      pixels.setPixelColor(i, Wheel(((i * 256 / pixels.numPixels()) + rainbowCycleCycles) & 255));
+    }
+    pixels.show();
+
+  rainbowCycleCycles++;
+  if(rainbowCycleCycles >= 256*5) rainbowCycleCycles = 0;
+}
+
+
+void theaterChase(uint32_t c) {
+  for (int i=0; i < pixels.numPixels(); i=i+3) {
+      pixels.setPixelColor(i+theaterChaseQ, c);    
+    }
+    pixels.show();
+    for (int i=0; i < pixels.numPixels(); i=i+3) {
+      pixels.setPixelColor(i+theaterChaseQ, 0);        
+    }
+    theaterChaseQ++;
+    if(theaterChaseQ >= 3) theaterChaseQ = 0;
+}
+
+
+
+void theaterChaseRainbow() {
+  for (int i=0; i < pixels.numPixels(); i=i+3) {
+    pixels.setPixelColor(i+theaterChaseRainbowQ, Wheel( (i+theaterChaseRainbowCycles) % 255));    
+  }
+      
+  pixels.show();
+  for (int i=0; i < pixels.numPixels(); i=i+3) {
+    pixels.setPixelColor(i+theaterChaseRainbowQ, 0);              
+  }      
+  theaterChaseRainbowQ++;
+  theaterChaseRainbowCycles++;
+  if(theaterChaseRainbowQ >= 3) theaterChaseRainbowQ = 0;
+  if(theaterChaseRainbowCycles >= 256) theaterChaseRainbowCycles = 0;
+}
+
 
 uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
   if(WheelPos < 85) {
-   return MagicLED.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-  } else if(WheelPos < 170) {
-   WheelPos -= 85;
-   return MagicLED.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  } else {
-   WheelPos -= 170;
-   return MagicLED.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+    return pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3);
   }
-}
-
-void utikej(uint32_t c, uint8_t wait) {
-  for (int j=0; j<10; j++) {  
-    for (int q=0; q < 3; q++) {
-      for (int i=0; i < MagicLED.numPixels(); i=i+3) {
-        MagicLED.setPixelColor(i+q, c);   
-      }
-      MagicLED.show();
-     
-      delay(interval);
-     
-      for (int i=0; i < MagicLED.numPixels(); i=i+3) {
-        MagicLED.setPixelColor(i+q, 0);     
-      }
-    }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
   }
-}
-
-void vymaluj(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<MagicLED.numPixels(); i++) {
-      MagicLED.setPixelColor(i, c);
-      MagicLED.show();
-      delay(interval);
-  }
-
-}
-
-void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<MagicLED.numPixels(); i++) {
-    MagicLED.setPixelColor(i, c);
-    MagicLED.show();
-    delay(wait);
-  }
-}
-
-void rainbowCycle(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256*5; j++) {
-    for(i=0; i< MagicLED.numPixels(); i++) {
-      MagicLED.setPixelColor(i, Wheel(((i * 256 / MagicLED.numPixels()) + j) & 255));
-    }
-    MagicLED.show();
-    delay(wait);
-  }
-}
-
-void theaterChase(uint32_t c, uint8_t wait) {
-  for (int j=0; j<10; j++) {
-    for (int q=0; q < 3; q++) {
-      for (int i=0; i < MagicLED.numPixels(); i=i+3) {
-        MagicLED.setPixelColor(i+q, c);
-      }
-      MagicLED.show();
-
-      delay(wait);
-
-      for (int i=0; i < MagicLED.numPixels(); i=i+3) {
-        MagicLED.setPixelColor(i+q, 0);
-      }
-    }
-  }
+  WheelPos -= 170;
+  return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
